6a509de31df84cc1dac37cf7135f326a8f2d24b8
Clinton Halpin <chalpin@alpha-sense.com>
2022-02-19 15:38:43 -0500

    update

--- setup.sh
+++ setup.sh
@@ -1,7 +1,7 @@
 echo "" >> ~/.bashrc
 echo "
 function parse_git_branch() {
-        git branch 2>/dev/null | grep '^*' | colrm 1 2
+        git branch 2> /dev/null | sed -e '/^[^*]/d' -e 's/* \(.*\)/ (\1)/'
 }
 " >> ~/.bashrc
-echo 'export PS1="> \]\w\[\] ($(parse_git_branch)\[\]) $ "' >> ~/.bashrc
+echo '>:: \]\w\[\] $(parse_git_branch)\[\] $ ' >> ~/.bashrc
