25c930a55891137e43f097018d546ed7d21cc63c
Clinton Halpin <chalpin@alpha-sense.com>
2022-02-20 16:50:41 -0500

    this never works

--- setup.sh
+++ setup.sh
@@ -1,3 +1,6 @@
 echo "" >> ~/.bashrc
-echo "parse_git_branch() { git branch 2> /dev/null | sed -e '/^[^*]/d' -e 's/* \(.*\)/ (\1)/' }" >> ~/.bashrc
+echo "parse_git_branch() { 
+ git branch 2> /dev/null | sed -e '/^[^*]/d' -e 's/* \(.*\)/(\1)/'
+}" >> ~/.bashrc
+echo "" >> ~/.bashrc
 echo 'export PS1=">:: \[\033[32m\]\w - \[\e[0;33m\]\$(parse_git_branch)\[\033[00m\] $ "' >> ~/.bashrc
