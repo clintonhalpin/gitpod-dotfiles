eea7b59886351e5240b8a058e4763e549f7c90a9
Clinton Halpin <chalpin@alpha-sense.com>
2022-02-24 17:20:21 -0500

    feat: update

--- setup.sh
+++ setup.sh
@@ -1,2 +1,7 @@
-file_contents=$(cat bashrc.txt)
-echo $file_contents >> ~/.bashrc
+PARSE_BRANCH="parse_git_branch() { 
+    git branch 2> /dev/null | sed -e '/^[^*]/d' -e 's/* \(.*\)/(\1)/'
+}"
+PS1='>:: \[\033[32m\]\w - \[\e[0;33m\]\$(parse_git_branch)\[\033[00m\] $ '
+
+echo $PARSE_BRANCH >> ~/.bashrc
+echo $PS1 >> ~/.bashrc
