2e9452eeefc8cbec03cae586678bca1eb34bbefc
Clinton Halpin <chalpin@alpha-sense.com>
2022-02-25 08:28:05 -0500

    1 getting escaped

--- setup.sh
+++ setup.sh
@@ -1,5 +1,5 @@
 PARSE_BRANCH="parse_git_branch() { 
-    git branch 2> /dev/null | sed -e '/^[^*]/d' -e 's/* \(.*\)/(\1)/'
+    git branch 2> /dev/null | sed -e '/^[^*]/d' -e 's/* \(.*\)/(\1\)/'
 }"
 PS1='export PS1=">:: \[\033[32m\]\w - \[\e[0;33m\]\$(parse_git_branch)\[\033[00m\] $ "'
 
