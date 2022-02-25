c05b0f0d8c4e4bd5f897cb00acb6dac7341b8d47
Clinton Halpin <chalpin@alpha-sense.com>
2022-02-25 08:19:14 -0500

    update again

--- setup.sh
+++ setup.sh
@@ -3,10 +3,8 @@ PARSE_BRANCH="parse_git_branch() {
 }"
 PS1='export PS1=">:: \[\033[32m\]\w - \[\e[0;33m\]\$(parse_git_branch)\[\033[00m\] $ "'
 
+echo "" >> ~/.bashrc
 echo $PARSE_BRANCH >> ~/.bashrc
+echo "" >> ~/.bashrc
 echo $PS1 >> ~/.bashrc
-
-
-parse_git_branch() { 
-    git branch 2> /dev/null | sed -e '/^[^*]/d' -e 's/* \(.*\)/(\1)/'
-}
+echo "" >> ~/.bashrc
